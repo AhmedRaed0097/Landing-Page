59da409852e0bad1f6303b10ee71efd7e25339c4
ahmedraed0097 <ahmedraed0097@gmail.com>
2022-02-27 09:55:16 +0300

    use core components in home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,41 @@
 <template>
-  <div style="height: 2000px;">
+  <div style="height: 8000px">
     <!--  -->
-    <Header />
+    <Header class="component" />
+    <OurServices class="component" />
+    <RecentWork class="component" />
+    <Carousel class="component" :objectType="'qoutes'" />
+    <OurBlog class="component" />
+    <PricingTables class="component" />
+    <OurTeam />
+    <Carousel class="component" :objectType="'brands'" />
   </div>
 </template>
 
 <script>
-import Header from '@/components/core/Header'
+import PricingTables from "@/components/core/PricingTables";
+import OurServices from "@/components/core/OurServices";
+import RecentWork from "@/components/core/RecentWork";
+import Carousel from "@/components/core/Carousel";
+import OurTeam from "@/components/core/OurTeam";
+import OurBlog from "@/components/core/OurBlog";
+import Header from "@/components/core/Header";
 export default {
-  components:{
-    Header
-  }
-}
+  components: {
+    PricingTables,
+    OurServices,
+    RecentWork,
+    OurTeam,
+    Carousel,
+    OurTeam,
+    OurBlog,
+    Header,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+.component {
+  margin: 240px  0 !important;
+}
 </style>
